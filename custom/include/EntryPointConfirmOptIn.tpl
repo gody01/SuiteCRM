f61fdcf707fefa12494fbe02acc39d5deb628c14
Matjaž Godec <matjaz.godec@gmail.com>
2019-02-22 14:35:16 +0100

    Spremembe na sugar.agenda.si - custom ...
     On branch sugar.agenda.si
     Your branch is up to date with 'origin/sugar.agenda.si'.
    
     Changes to be committed:
    	new file:   custom/include/EntryPointConfirmOptIn.tpl
    	new file:   custom/include/SuiteEditor/SuiteEditorSettingsForTinyMCE.php
    	new file:   custom/include/images/company_logo.png
    	new file:   custom/include/language/sl_si.lang.php
    	new file:   custom/themes/default/images/company_logo.png

---
+++ custom/include/EntryPointConfirmOptIn.tpl
@@ -0,0 +1,21 @@
+<html>
+<meta http-equiv="refresh" content="0; url=http://www.agenda.si/potrdilo-eposte/?email={$FOCUS->email_address}">
+<body>
+
+<center>
+<!---
+{if $FOCUS}
+    <div class="email-address-confirmed">
+        Hvala za potrditev prejemanja obvestil na elektronski naslov "{$FOCUS->email_address}" 
+    </div>
+{else}
+    <div class="error">
+        Elektronskega naslova ni bilo mogoče potrditi.
+    </div>
+{/if}
+
+Preusmerili vas bomo na naš <a href="http://www.agenda.si/">spletni naslov</a>, kjer si lahko ogledate več informacij o nas.
+</center>
+-->
+</body>
+</html>
---
+++ custom/include/language/sl_si.lang.php
@@ -0,0 +1,70 @@
+<?php
+$GLOBALS['app_list_strings']['sales_probability_dom']=array ( '' => '',
+  'Prospecting' => '5',
+  'Qualification' => '25',
+  'Perception Analysis' => '50',
+  'Negotiation/Review' => '75',
+  'Closed Won' => '100',
+  'Closec Lost' => '0',
+);
+$GLOBALS['app_list_strings']['account_type_dom'] =array (
+'Competitor' => 'Konkurenca',
+'Customer'   => 'Kupec',
+'Integrator' => 'Integrator',
+'Partner'    => 'Partner',
+'Press'      => 'Mediji',
+'Prospect'   => 'Potencialni kupec',
+'Reseller'   => 'Prodajalec',
+'Other'      => 'Drugo',
+);
+$GLOBALS['app_list_strings']['lead_source_dom'] = array (
+'Cold Call'                                        => 'Hladen klic',
+'Existing Customer'                                => 'Obstoječ kupec',
+'Employee'                                         => 'Zaposleni',
+'Partner'                                          => 'Partnerji',
+'Conference'                                       => 'Konferenca',
+'Trade Show'                                       => 'Sejem',
+'Web Site'                                         => 'Internet (web-mail)',
+'Word of mouth'                                    => 'Od ust do ust',
+'Campaign'                                         => 'Kampanje',
+'Other'                                            => 'Drugo',
+);
+$GLOBALS['app_list_strings']['industry_dom'] = array (
+'Banking'                                          => 'Banke/Finance/Zavarovalnice',
+'Chemicals'                                        => 'Kemična',
+'Communications'                                   => 'Telekomunikacije',
+'Construction'                                     => 'Gradbeništvo',
+'Education'                                        => 'Izobraževanje',
+'Energy'                                           => 'Energetika',
+'Engineering'                                      => 'Strojegradnja',
+'Entertainment'                                    => 'Turizem',
+'Government'                                       => 'Državna uprava',
+'Healthcare'                                       => 'Zdravstvo',
+'Manufacturing'                                    => 'Proizvodnja',
+'Media'                                            => 'Mediji',
+'Not For Profit'                                   => 'Neprofitne',
+'Retail'                                           => 'Trgovina',
+'Transportation'                                   => 'Logistika',
+'Utilities'                                        => 'Storitve',
+'Other'                                            => 'Drugo',
+);
+
+$GLOBALS['app_list_strings']['duration_intervals']=array (
+  0 => '00',
+  2 => '02',
+  5 => '05',
+  10 => '10',
+  15 => '15',
+  30 => '30',
+  45 => '45',
+);
+
+$GLOBALS['app_list_strings']['sales_stage_dom']=array (
+  'Prospecting' => 'Iskanje strank',
+  'Qualification' => 'Ocena ustr./vredn./sprejemlj.',
+  'Perception Analysis' => 'Poslana ponudba',
+  'Negotiation/Review' => 'Pogajanja',
+  'Closed Won' => 'Dobljeno',
+  'ClosedWonValue' => 'Dobljeno Vrednost',
+  'Closed Lost' => 'Izgubljeno',
+);
